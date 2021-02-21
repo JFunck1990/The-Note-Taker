210a80e85b76e191aeaacfb684cc397a68f183a3
Jack Funck <jackfunck21@gmail.com>
2021-02-21 14:58:12 -0500

    setting up the api post rout

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
+//Dependencies
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
 
-// initialize express
+// initialize express app
 const app = express();
 const PORT = process.env.Port || 3000;
 
+// sets up express app for data parsing
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', function(req,res) {
+//Routs
+
+app.get('/', (req,res) => {
 	res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.get('/notes', function(req,res) {
+app.get('/notes', (req,res) => {
 	res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
+app.get("/api/notes", (req,res) => {
+    res.json(notes);
+});
+// setting up api post rout
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+} );
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
