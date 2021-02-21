44e4bcfe242c0d3e77e356b6883875a394946760
Jack Funck <jackfunck21@gmail.com>
2021-02-21 13:51:03 -0500

    got the PORT to listen

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const path = require('path');
 
 // initialize express
 const app = express();
-const port = process.env.Port || 3000;
+const PORT = process.env.Port || 3000;
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
