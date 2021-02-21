5bab284c7b553e382f7933b3420b87ad6e3d12d1
Jack Funck <jackfunck21@gmail.com>
2021-02-21 14:00:18 -0500

    adding get's

--- server.js
+++ server.js
@@ -10,4 +10,12 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.get('/', function(req,res) {
+	res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', function(req,res) {
+	res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
