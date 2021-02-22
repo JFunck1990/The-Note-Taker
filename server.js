4d2083bca3fc5e61a9a16a1aa86a859542d91c10
Jack Funck <jackfunck21@gmail.com>
2021-02-22 15:54:33 -0500

    fixed error

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ fs.readFile("db/db.json", "utf8", (err, data) => {
 // Routs
 
 app.get('/', (req,res) => {
-	res.sendFile(path.join(__dirname, '/public/index.html'));
+	res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.get('/notes', (req,res) => {
-	res.sendFile(path.join(__dirname, '/public/notes.html'));
+	res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get("/api/notes", (req,res) => {
@@ -50,6 +50,6 @@ app.post("/api/notes", (req, res) => {
  //fs.writeFile("db/db.json",JSON.stringify(theNotes), err => {
    //  if (err) throw err;
  //});
-//});
+});
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
