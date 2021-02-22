df7bef0e1a5afe9f69cd672fbedae671e04c081c
Jack Funck <jackfunck21@gmail.com>
2021-02-22 09:37:31 -0500

    fixed

--- server.js
+++ server.js
@@ -7,28 +7,31 @@ const path = require('path');
 const app = express();
 const PORT = process.env.Port || 3000;
 
-fs.readFile("db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
-
-    const notes = JSON.parse(data);
-
-    console.log("this is the notes" + notes);
 
 // sets up express app for data parsing
     app.use(express.urlencoded({extended: true}))
     app.use(express.json());
     app.use(express.static(path.join(__dirname, '/public')));
 
-//Routs
 
+//reading the db.json file
+fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+
+// Parses TheNotes as a string and returns ad a object
+    const theNotes = JSON.parse(data);
+
+    console.log("this is the notes" + notes);
 
 //need to ask to make sure it is *
+// Routs
+
 app.get('*', (req,res) => {
-	res.sendFile(path.join(__dirname, './public/index.html'));
+	res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req,res) => {
-	res.sendFile(path.join(__dirname, './public/notes.html'));
+	res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get("/api/notes", (req,res) => {
@@ -38,9 +41,12 @@ app.get("/api/notes", (req,res) => {
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
 
-    notes.push(newNote);
+    theNotes.push(newNote);
 
-} );
+
+    res.json(newNote);
+});
+
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
